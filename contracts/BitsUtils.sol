7bfa363d81389dbf53dadc70bd2650ff9908828a
CJ42 <cavallerajean@gmail.com>
2021-12-03 00:05:55 +0000

    split functions between Solidity and Assembly versions

--- contracts/BitsUtils.sol
+++ contracts/BitsUtils.sol
@@ -6,128 +6,106 @@ pragma solidity ^0.7.0;
 /// @dev most functions are implemented with a low-level Assembly mode for gas optimisation
 library BitsUtils {
     
-    function isOdd(uint x, bool assemblyMode) internal pure returns (bool) {
-        if (assemblyMode) {
-            assembly {
-                mstore(0x00, and(x, 1))
-                return(0x00, 32)
-            }
-        } else {
-            return (x & 1) == 1;
-        }
+    /// @dev check if an unsigned integer is odd or even
+    /// @param input the number to check
+    function isOdd(uint256 input) internal pure returns (bool) {
+        return (input & 1) == 1;
     }
     
 
-    /// #2.1 Test if the n-th bit is set.
-    function isNthBitSet(uint x, uint n, bool assemblyMode) internal pure returns (bool) {
-        if (assemblyMode) {
-            assembly {
-                mstore(0x00, and(x, shl(1, n)))
-                return(0x00, 32)
-            }
-        } else {
-            // if the nth bit is set:
-            //    -> anding will eliminate all the bits EXCEPT the nth bit (so will return a number)
-            // else:
-            //    -> anding will eliminate ALL the bits (so will return 0)
-            return x & (1 << n) != 0 ? true : false;
-        }
+    /// @dev check if the nth bit is set in a 256 bits unsigned integer
+    /// @param input the number to check
+    /// @param nthBit the bit number to check in the binary representation of the number
+    function isNthBitSet(uint256 input, uint256 nthBit) internal pure returns (bool) {
+        // if the nth bit is set:
+        //    -> anding will eliminate all the bits EXCEPT the nth bit (so will return a number)
+        // else:
+        //    -> anding will eliminate ALL the bits (so will return 0)
+        return input & (1 << nthBit) != 0 ? true : false;
     }
     
-    // TODO: isNthBitSet for signed integers
-    
-    /// #3 Set the n-th bit.
-    function setNthBit(uint x, uint n, bool assemblyMode) internal pure returns (uint) {
-        if (assemblyMode) {
-            assembly {
-                mstore(0x00, or(x, shl(n, 1)))
-                return(0x00, 32)
-            }
-        } else {
-            return x | (1 << n);
-        }
+    /// @dev set the `nthBit` to 1 if it was set to 0
+    /// @param input a uint256 number
+    /// @param nthBit the bit number to set in `input`, counting from the right
+    function setNthBit(uint256 input, uint256 nthBit) internal pure returns (uint256) {
+        return input | (1 << nthBit);
     }
     
-    
-    // TODO: setNthBitSet for signed integers
-    
-    /// #4 Unset the n-th bit.
-    function unsetNthBit(uint x, uint n, bool assemblyMode) internal pure returns (uint) {
-        if (assemblyMode) {
-            assembly {
-                mstore(0x00, and(x, not(shl(n, 1))))
-                return(0x00, 32)
-            }
-        } else {
-            return x & ~(1 << n);
-        }
+    function unsetNthBit(uint256 input, uint256 nthBit) internal pure returns (uint256) {
+        return input & ~(1 << nthBit);
     }
     
-    /// #5 Toggle the n-th bit
-    function toggleNthBit(uint x, uint n, bool assemblyMode) internal pure returns (uint) {
-        if (assemblyMode) {
-            assembly {
-                mstore(0x00, xor(x, shl(n, 1)))
-                return(0x00, 32)
-            }
-        } else {
-            return x ^ (1 << n);
-        }
-        
-    
-        
+    function toggleNthBit(uint256 input, uint256 nthBit) internal pure returns (uint256) {
+        return input ^ (1 << nthBit);
     }
     
-    /// #6 Turn off the rightmost 1-bit
-    function toggleRightmost1Bit(uint x, bool assemblyMode) internal pure returns (uint) {
-        if (assemblyMode) {
-            assembly {
-                mstore(0x00, and(x, sub(x, 1)))
-                return(0x00, 32)
-            }
-        } else {
-            return x & (x - 1);
-        }
-        
+    function toggleRightmost1Bit(uint256 input) internal pure returns (uint256) {
+        return input & (input - 1);        
     }
     
     
-    /// #7. Isolate the rightmost 1-bit.
-    // function isolateRightmostBit(uint x) internal pure returns (uint) {
-        
-    // }
-    
-    
-    
-    /// #8. Right propagate the rightmost 1-bit.
-    // TODO: this does not always return the expected result. Need more deep dive
-    // NOTE: Beaware for 'x' = 0, all the bits will be turned to 1.
-    // it will then overflow and return the biggest uint possible (2²⁵⁶-1)
-    function rightPropagateRightmost1Bit(uint x, bool assemblyMode) internal pure returns (uint) {
-        if (assemblyMode) {
-            assembly {
-                mstore(0x00, or(x, sub(x, 1)))
-                return(0x00, 32)
-            }
-        } else {
-            return x | (x - 1);
-        }
-    }
-    
-    // TODO: rightPropagateRightmostBit for signed integers
-    
-    
-    /// #9. Isolate the rightmost 0-bit.
-    // TODO: assembly mode
-    function isolateRightmost0Bit(uint x) internal pure returns (uint) {
-        return ~x & (x + 1);
+    /// @dev this does not always return the expected result. Need more deep dive
+    ///     Beaware if 'input' = 0, all the bits will be turned to 1.
+    /// it will then overflow and return the biggest uint256 possible (2 ** 256 -1)
+    function rightPropagateRightmost1Bit(uint256 input) internal pure returns (uint256) {
+        return input | (input - 1);
     }
     
     
-    /// #10. Turn on the rightmost 0-bit.
-    // TODO: assembly mode
-    function turnOnRightmost0Bit(uint x) internal pure returns (uint) {
-        return x | (x + 1);
+    function isolateRightmost0Bit(uint256 input) internal pure returns (uint256) {
+        return ~input & (input + 1);
+    }
+    
+    function turnOnRightmost0Bit(uint256 input) internal pure returns (uint256) {
+        return input | (input + 1);
     }
 
+    // Optimised version of the functions, using assembly and YUL
+    // use this caution
+
+    function isOddAsm(uint256 input) internal pure returns (bool) {
+        assembly {
+            mstore(0x00, and(input, 1))
+            return(0x00, 32)
+        }
+    }
+
+    function isNthBitSetAsm(uint256 input, uint256 nthBit) internal pure returns (bool) {
+        // if the nth bit is set:
+        //    -> anding will eliminate all the bits EXCEPT the nth bit (so will return a number)
+        // else:
+        //    -> anding will eliminate ALL the bits (so will return 0)
+        assembly {
+            mstore(0x00, and(input, shl(1, nthBit)))
+            return(0x00, 32)
+        }   
+    }
+
+    function setNthBitAsm(uint256 input, uint256 nthBit) internal pure returns (uint256) {
+        assembly {
+            mstore(0x00, or(input, shl(nthBit, 1)))
+            return(0x00, 32)
+        }
+    }
+
+    function unsetNthBitAsm(uint256 input, uint256 nthBit) internal pure returns (uint256) {
+        assembly {
+            mstore(0x00, and(input, not(shl(nthBit, 1))))
+            return(0x00, 32)
+        }
+    }
+
+    function toggleNthBitAsm(uint256 input, uint256 nthBit) internal pure returns (uint256) {
+        assembly {
+            mstore(0x00, xor(input, shl(nthBit, 1)))
+            return(0x00, 32)
+        }        
+    }
+
+    function toggleRightmost1BitAsm(uint256 input) internal pure returns (uint256) {
+        assembly {
+            mstore(0x00, and(input, sub(input, 1)))
+            return(0x00, 32)
+        }
+    }
 }
